2cb285baf9f63b03f7b58055db11bbc4ef0e6e2d
L0rdSaiyan <89421533+L0rdSaiyan@users.noreply.github.com>
2022-01-26 15:02:58 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+	
+	String Vegetto = "Esta é a fusão Vegetto!";
+	System.out.println(Vegetto);
+    Vegetto = Vegetto.replace("Vegetto","Gogeta");
+    System.out.println(Vegetto);
+
+	}
+}
